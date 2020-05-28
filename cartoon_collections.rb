aac6ede1dcdd1b8e92797f35b51d07255afc50fb
Samantha Wabi <swfullpotential@gmail.com>
2020-05-28 23:40:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |dwarf, index| puts "#{index + 1} #{dwarf}" }
 end
 
-def summon_captain_planet
+def summon_captain_planet(array)
   array.collect { |w| "#{w.capitalize}!"}
 end
 
